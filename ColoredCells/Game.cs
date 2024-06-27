6fcf95df1d2877302bbb99e2afe766c9b9a8d184
asazary <ars.sazary@gmail.com>
2024-06-27 20:11:34 +0300

    Add project files.

---
+++ ColoredCells/Game.cs
@@ -0,0 +1,159 @@
+﻿namespace ColoredCells
+{
+    internal enum GameState
+    {
+        Continues = 0,
+        Paused,
+        Finished
+    }
+
+    internal class Game
+    {
+        public int[,] Field { get; private set; } = new int[1, 1] { { 0 } };
+        public int Size { get; private set; } = 3;
+        public int Level { get; private set; } = 1;
+        public int ColorsCount { get; private set; } = 2;
+        
+        public GameState GameStatus { get; private set; } = GameState.Finished;
+        private readonly int _randomMovesCount = 1;
+
+        private static readonly Func<int, int, int>[] AllCellFuncs =
+        [
+            (x, clrCnt) => x + 1 <= clrCnt ? x + 1 : 1,             // level 0
+            (x, clrCnt) => x - 1 >= 1 ? x - 1 : clrCnt,             // level 1
+            (x, clrCnt) => x + 2 <= clrCnt ? x + 2 : x + 2 - clrCnt,// level 2            
+            (x, clrCnt) => x + 1 <= clrCnt ? x + 1 : 1,             // level 3
+            (x, clrCnt) => x - 2 >= 1 ? x - 2 : clrCnt + x - 2      // level 4            
+        ];
+
+        private readonly List<Action<int, int, List<CellCoords>>> AllLevelFuncs = [];
+        private readonly Action<int, int, List<CellCoords>> GameLevelFuncs = delegate { };
+
+
+        public Game(int size, int colorsCount, int level, int randomMovesCount)
+        {
+            Size = size;
+            Field = new int[size, size];
+            Level = level;
+            ColorsCount = colorsCount;
+            _randomMovesCount = randomMovesCount;
+
+            AllLevelFuncs.Add(ApplyLevel0);
+            AllLevelFuncs.Add(ApplyLevel1);
+            AllLevelFuncs.Add(ApplyLevel2);
+            AllLevelFuncs.Add(ApplyLevel3);
+            AllLevelFuncs.Add(ApplyLevel4);
+
+            for (var i = 0; i <= level; i++)
+                GameLevelFuncs += AllLevelFuncs[i];            
+        }
+
+        public void StartGame()
+        {
+            GameStatus = GameState.Paused;
+            InitCellsWithRandomValue();
+            DoRandomMoves();
+            GameStatus = GameState.Continues;
+        }
+        public void EndGame() => GameStatus = GameState.Finished;
+        public void PauseGame() => GameStatus = GameState.Paused;
+        public void ContinueGame() => GameStatus = GameState.Continues;
+
+        public bool CheckWin()
+        {
+            var val = Field[0, 0];
+            for (var i = 0; i < Size; i++)
+                for (var j = 0; j < Size; j++)
+                    if (Field[i, j] != val) return false;
+            return true;
+        }
+
+        public List<CellCoords> ChangeCell(CellCoords coords)
+        {
+            if (GameStatus != GameState.Continues && 
+                  GameStatus != GameState.Paused) return [];
+
+            var (row, col) = (coords.Row, coords.Col);
+            var changedCells = new List<CellCoords>();
+            GameLevelFuncs(row, col, changedCells);
+            return changedCells;
+        }
+
+        public void InitCellsWithRandomValue() 
+        {
+            var initValue = new Random().Next(1, ColorsCount + 1);
+            for (var i = 0; i < Size; i++)
+                for (var j = 0; j < Size; j++)
+                    Field[i, j] = initValue;
+        }
+
+
+        public void DoRandomMoves()
+        {
+            var rand = new Random();
+            for (var i = 0; i < _randomMovesCount; i++)
+            {
+                var row = rand.Next(Size);
+                var col = rand.Next(Size);
+                ChangeCell(new CellCoords(row, col));
+            }
+        }
+
+        private void ApplyLevel0(int row, int col, List<CellCoords> changedCells)
+        {
+            Field[row, col] = AllCellFuncs[0](Field[row, col], ColorsCount);
+            changedCells.Add(new CellCoords(row, col));
+        }            
+
+        private void ApplyLevel1(int row, int col, List<CellCoords> changedCells)
+        {
+            for (var j = 0; j < Size; j++)
+            {
+                if (j == col) continue;
+                Field[row, j] = AllCellFuncs[1](Field[row, j], ColorsCount);
+                changedCells.Add(new CellCoords(row, j));
+            }
+        }
+
+        private void ApplyLevel2(int row, int col, List<CellCoords> changedCells)
+        {
+            for (var i = 0; i < Size; i++)
+            {
+                if (i == row) continue;
+                Field[i, col] = AllCellFuncs[2](Field[i, col], ColorsCount);
+                changedCells.Add(new CellCoords(i, col));
+            }
+        }
+
+        private void ApplyLevel3(int row, int col, List<CellCoords> changedCells)
+        {
+            for (var i = 1; row - i >= 0 && col + i < Size; i++)
+            {
+                Field[row - i, col + i] = AllCellFuncs[3](Field[row - i, col + i], ColorsCount);
+                changedCells.Add(new CellCoords(row - i, col + i));
+            }
+                
+            for (var i = 1; row + i < Size && col - i >= 0; i++)
+            {
+                Field[row + i, col - i] = AllCellFuncs[3](Field[row + i, col - i], ColorsCount);
+                changedCells.Add(new CellCoords(row + i, col - i));
+            }
+                
+        }
+
+        private void ApplyLevel4(int row, int col, List<CellCoords> changedCells)
+        {
+            for (var i = 1; row + i < Size && col + i < Size; i++)
+            {
+                Field[row + i, col + i] = AllCellFuncs[4](Field[row + i, col + i], ColorsCount);
+                changedCells.Add(new CellCoords(row + i, col + i));
+            }
+                
+            for (var i = 1; row - i >= 0 && col - i >= 0; i++)
+            {
+                Field[row - i, col - i] = AllCellFuncs[4](Field[row - i, col - i], ColorsCount);
+                changedCells.Add(new CellCoords(row - i, col - i));
+            }                
+        }
+    }
+}
---
+++ ColoredCells/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ColoredCells
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+        private int _size = 3;
+
+        private const int BlockBorder = 2;
+        private readonly Brush _blockBorderBrush = Brushes.Gray;
+
+        private Dictionary<int, Brush> _blockBrushes = new()
+        {
+            { 0, Brushes.White },
+            { 1, Brushes.LightSeaGreen },
+            { 2, Brushes.RoyalBlue },
+            { 3, Brushes.IndianRed },
+            { 4, Brushes.YellowGreen },
+            { 5, Brushes.DarkViolet }
+        };
+
+        private Rectangle[,] Cells = new Rectangle[0, 0];
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _game = new Game(_size, 2, 1, 1);
+            Cells = new Rectangle[_size, _size];
+
+            FieldSizeList.Items.Clear();
+            FieldSizeList.ItemsSource = new int[] { 3, 4, 5 }; 
+            FieldSizeList.SelectedIndex = 0;
+
+            GameLevelList.Items.Clear();
+            GameLevelList.ItemsSource = new int[] { 1, 2, 3, 4 };
+            GameLevelList.SelectedIndex = 0;
+
+            ColorsCountList.Items.Clear();
+            ColorsCountList.ItemsSource = new int[] { 2, 3, 4 };
+            ColorsCountList.SelectedIndex = 1;
+
+            FieldCanvas.Background = Brushes.LightGray;           
+        }
+
+        private void MainWindowOnLoad(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e) => StartNewGame();
+
+        private void StartNewGame()
+        {
+            _size = (int)FieldSizeList.SelectedItem;
+            var level = (int)GameLevelList.SelectedItem;
+            var colorsCount = (int)ColorsCountList.SelectedItem;
+            var randomMoves = (int)RandMovesCntSlider.Value;
+
+            _game = new Game(_size, colorsCount, level, randomMoves);
+            Cells = new Rectangle[_size, _size];
+                        
+            _game.StartGame();
+            WinLabel.Visibility = Visibility.Collapsed;
+            DrawField(_size);
+        }
+
+        private void FinishGame()
+        {
+            _game.EndGame();
+            WinLabel.Visibility = Visibility.Visible;
+        }
+
+        private void DrawField(int size)
+        {
+            FieldCanvas.Children.Clear();
+            var actWidth = FieldCanvas.ActualWidth;
+
+            var blockWidth = actWidth / size;
+            var blockSideSize = blockWidth;            
+
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = 0; j < size; j++)
+                {
+                    var rect = CreateBlock(blockSideSize, i, j);
+                    FieldCanvas.Children.Add(rect);
+                    Cells[i, j] = rect;
+                    Canvas.SetTop(rect, i * blockSideSize);
+                    Canvas.SetLeft(rect, j * blockSideSize);
+                }
+            }
+        }
+
+        private Rectangle CreateBlock(double blockSideSize, int row, int col)
+        {
+            var cellValue = _game.Field[row, col];
+            var rect = new Rectangle
+            {
+                Fill = _blockBrushes[cellValue],  // _blockZeroBrush,
+                Stroke = _blockBorderBrush,
+                StrokeThickness = BlockBorder,
+                Height = blockSideSize,
+                Width = blockSideSize,
+                Tag = new CellCoords { Row = row, Col = col }
+            };
+            rect.MouseLeftButtonDown += Block_OnClick;
+            
+            return rect;
+        }
+
+        private void Block_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Rectangle rect)
+                ChangeCell((CellCoords)rect.Tag);
+        }
+
+        private void ChangeCell(CellCoords cellCoords)
+        {
+            if (_game.GameStatus != GameState.Continues) return;
+            _game.PauseGame();
+
+            var changedCells = _game.ChangeCell(cellCoords);
+            ChangeCellsColors(changedCells);
+            if (_game.CheckWin()) FinishGame();
+        }
+
+        private void ChangeCellsColors(IEnumerable<CellCoords> cellsCoords)
+        {
+            foreach (var cell in cellsCoords)
+            {
+                Cells[cell.Row, cell.Col].Fill = _blockBrushes[_game.Field[cell.Row, cell.Col]];
+            }
+
+            _game.ContinueGame();
+        }                
+    }
+
+    public struct CellCoords(int row, int col)
+    {
+        public int Row = row;
+        public int Col = col;
+
+        public override readonly string ToString() => $"{Row}, {Col}";
+    }
+}
